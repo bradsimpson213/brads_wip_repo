7b4df7bc63bfb28f87b08f793d5b93ebf239ad51
Brad Simpson <bradsimpson@icloud.com>
2025-11-19 12:44:16 -0500

    working on recursion lecture

---
+++ recursion.py
@@ -0,0 +1,37 @@
+# Recursion in programming is a technique where a function calls itself, either
+# directly or indirectly, to solve a problem. It involves breaking down a
+# complex problem into smaller, similar sub-problems until a simple,
+# non-recursive case (known as the base case) is reached.
+
+
+foods = ["pizza", "hamburger", "salad", "chicken tenders", "apple pie"]
+
+def recursive_iterator(list_obj):
+    print("I RAN")
+
+    if len(list_obj) == 0:
+        return
+    
+    print(list_obj[0])
+    
+    return recursive_iterator(list_obj[1:])
+
+
+# recursive_iterator(foods)
+
+# Fibonacci Sequence
+# 0 1 1 2 3 5 8 13 21 34 55 ...
+
+def fibonacci_recursive(n):
+    # print("I RAN", n)
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer.")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+    
+
+print("The Fibonacci number is: ", fibonacci_recursive(35))
